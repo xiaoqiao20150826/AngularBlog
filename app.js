deef812666a792b9a2e17afdddeb6b20d5ea8e4b
kangil <elfmagic86@live.com>
2014-05-15 19:51:26 +0900

    부트스트랩을 사용하고 싶은데 정적리소스의 주소 바인딩이
    제대로 안된다. express에서 정적리소스 url 바인딩을 올바로 하는방법은?

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ var express = require('express')
   , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
-
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -27,8 +27,10 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/public',express.static(path.join(__dirname, 'public')));
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
