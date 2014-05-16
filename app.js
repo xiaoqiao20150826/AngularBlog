fa8ea2729a50c8c2c0ed5f7cfee8ebe1589f1ec0
kangil <elfmagic86@live.com>
2014-05-16 17:30:52 +0900

    express.static(...)이 정상동작하지만 문제가 생겼던 원인을
    못찾았다. 개발자모드 설정을 옮긴것과 /를 포함시킨것 등..실험해봐야함

--- app.js
+++ app.js
@@ -11,27 +11,36 @@ var express = require('express')
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-
-app.get('/', routes.index);
-app.get('/users', user.list);
-app.get('/public',express.static(path.join(__dirname, 'public')));
+setAllEnvironments(app);
+useAllEnvironments(app);
+//route url to any Response
+routeUrlToAnyResponse(app);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+//////helper
+function routeUrlToAnyResponse(app) {
+	app.get('/', routes.index);
+	app.get('/users', user.list);
+};
+function setAllEnvironments(app) {
+	app.set('port', process.env.PORT || 3000);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'ejs');
+};
+function useAllEnvironments(app) { //req에 대해서 선처리를 하는 미들웨어를 등록.
+	app.use(express.favicon());
+	app.use(express.logger('dev'));    //
+	app.use(express.bodyParser());     //form 전송시 분석.
+	app.use(express.methodOverride()); //post _method 골라줌.
+
+	app.use(app.router);
+	app.use('/resources',express.static(path.join(__dirname, '/public')));
+	
+	// development only
+	if ('development' == app.get('env')) {
+	  app.use(express.errorHandler());
+	}
+};
