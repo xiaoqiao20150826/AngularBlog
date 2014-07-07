7afd6711888dcbe97d8379a0056bebbc2d2f2edd
kangil <elfmagic86@live.com>
2014-07-07 14:16:27 +0900

    change README

--- common/Done.js
+++ common/Done.js
@@ -1,10 +1,27 @@
+/*
+ *  # 무엇.
+ *   비동기 함수의 인자로 전달되는 모든 callback을 대신하며, 그 것을 위한 편의 클래스
+ *   
+ *
+ *  TODO: [warn] getCallback(), getDataFn() 둘 모두 템플릿타입 체크를 하고 있다.  
+ *  TODO: [maybe] getCallback()을 조금 더 유연하게 만들 수 없을까? 리터럴을 타입클래스로?
+ *        동적타입언어이므로.. 타입체크 기능을 넣고 직접 코딩해야 할 듯.
+ */
+
 var U = require('./util/util.js');
 
 ////////////////////////////////Done 클래스 정의 시작
 var _NOMAL = 'nomalTemplate'
   , _ASYNC = 'asyncLoopTemplate';
 
-// 		생성자
+/* 	# 생성자
+ *    ## param 
+ *     - dataFn(data) : callback의 결과 에러가 없을 때, 호출되는 함수. data의 처리.
+ *     - errFn(err) : callback의 결과 에러가 있을 경우 호출되는 함수. err의 처리.
+ *     - templateType : 어떤 템플릿의 callback함수를 가져올지 결정. 
+ *    ## 호출형태
+ *     - new Done(dataFn) : 이때, errFn, templateType은 기본값을 할당한다.
+ */ 
 Done = module.exports = function Done(dataFn, errFn, templateType) {
 	if(!dataFn) throw new Error('dataFn은 필수').stack;
 	
