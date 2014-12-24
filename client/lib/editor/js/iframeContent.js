e13ef2cad9702e92d6420ce21d58034269efd55d
kangil <elfmagic86@live.com>
2014-12-24 19:01:10 +0900

    modify error of editor

--- client/lib/editor/js/iframeContent.js
+++ client/lib/editor/js/iframeContent.js
@@ -10,6 +10,7 @@
 		contentBody = contentWindow.document.body,
 		wysiwygHTML = '<html lang="ko"><head>'
 			+ '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">'
+			+ '<link rel="stylesheet" href="/resource/lib/editor/css/editorContent.css">'
 			+ '<title>Wygiwyg Panel</title>'
 			+ '</head>'
 			+ '<body id="wysiwyg" contenteditable="true">'
